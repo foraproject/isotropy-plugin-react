886bd7a639c6e0bde96216cf30f93c6590bcb3d8
Jeswin Kumar <jeswinpk@agilehead.com>
2015-12-15 23:15:37 +0530

    Type annotations

--- src/isotropy-plugin-react.js
+++ src/isotropy-plugin-react.js
@@ -1,20 +1,44 @@
 /* @flow */
-import { KoaContextType, KoaHandlerType } from "koa";
-import type KoaAppType from "koa";
-import { HttpMethodRouteOptionsType } from "isotropy-router";
+
+import type { KoaType, KoaContextType, KoaHandlerType } from "./flow/koa-types";
+import type { HttpMethodRouteOptionsType, HttpMethodRouteArgsType } from "isotropy-router";
 import Router from "isotropy-router";
-import React from "react";
 import reactAdapter from "isotropy-adapter-react";
 
-type ReactAppRouteType = {
+
+type HandlerRouteType = {
+    type: "handler",
+    url: string,
+    method: string,
+    handler: KoaHandlerType
+}
+
+type ReactComponentRouteType = {
+    type: "react",
     url: string,
     method: string,
     component: Function,
-    handler?: KoaHandlerType
+    args: Object,
+    context: KoaContextType,
+    options: HttpMethodRouteOptionsType
 }
 
+type RelayRouteType = {
+    type: "relay",
+    url: string,
+    method: string,
+    relayContainer: Function,
+    relayRoute: Object,
+    graphqlUrl: string,
+    args: Object,
+    context: KoaContextType,
+    options: HttpMethodRouteOptionsType
+}
+
+type AppRouteType = ReactComponentRouteType | HandlerRouteType | RelayRouteType;
+
 type ModuleType = {
-    routes: Array<ReactAppRouteType>
+    routes: Array<AppRouteType>
 }
 
 export type ReactAppType = {
@@ -25,9 +49,7 @@ export type ReactAppType = {
     template?: (html: string) => string
 };
 
-
 export type ReactConfigType = {}
-
 
 const getDefaultValues = function(val: Object = {}) : ReactAppType {
     return  {
@@ -38,35 +60,87 @@ const getDefaultValues = function(val: Object = {}) : ReactAppType {
     };
 };
 
+const getAppRoute = function(route: Object) : AppRouteType {
+    if (typeof route.handler !== "undefined" && route.handler !== null) {
+        return Object.assign({}, route, { type: "handler" });
+    } else if (typeof route.component !== "undefined" && route.component !== null) {
+        return Object.assign({}, route, { type: "react" });
+    } else if (typeof route.relayContainer !== "undefined" && route.relayContainer !== null) {
+        return Object.assign({}, route, { type: "relay" });
+    } else {
+        throw new Error("Unknown type. Route type must be handler, react or relay.");
+    }
+};
 
-const setup = async function(appConfig: ReactAppType, server: KoaAppType, config: ReactConfigType) : Promise {
+const getHandlerRoute = function(route: HandlerRouteType, appConfig: ReactAppType) : HttpMethodRouteArgsType {
+    return {
+        type: "pattern",
+        method: route.method,
+        url: route.url,
+        handler: route.handler
+    };
+};
+
+const getReactRoute = function(route: ReactComponentRouteType, appConfig: ReactAppType) : HttpMethodRouteArgsType {
+    return {
+        type: "pattern",
+        method: route.method,
+        url: route.url,
+        handler: async (context: KoaContextType, args: Object) => {
+            reactAdapter.render(
+                {
+                    component: route.component,
+                    args,
+                    context,
+                    options: {
+                        renderToStaticMarkup: appConfig.renderToStaticMarkup,
+                        template: appConfig.template || (x => x)
+                    }
+                }
+            );
+        },
+        options: { argumentsAsObject: true }
+    };
+};
+
+const getRelayRoute = function(route: RelayRouteType, appConfig: ReactAppType) : HttpMethodRouteArgsType {
+    return {
+        type: "pattern",
+        method: route.method,
+        url: route.url,
+        handler: async (context: KoaContextType, args: Object) => {
+            reactAdapter.renderRelayContainer(
+                {
+                    relayContainer: route.relayContainer,
+                    relayRoute: route.relayRoute,
+                    graphqlUrl: route.graphqlUrl,
+                    args,
+                    context,
+                    options: {
+                        renderToStaticMarkup: appConfig.renderToStaticMarkup,
+                        template: appConfig.template || ((x, props) => x)
+                    }
+                }
+            );
+        },
+        options: { argumentsAsObject: true }
+    };
+}
+
+const setup = async function(appConfig: ReactAppType, server: KoaType, config: ReactConfigType) : Promise {
     const router = new Router();
 
-    const routes = appConfig.module.routes.map(route => {
-        return (typeof route.handler !== "undefined" && route.handler !== null) ?
-            {
-                type: "pattern",
-                method: route.method,
-                url: route.url,
-                handler: route.handler
-            } :
-            {
-                type: "pattern",
-                method: route.method,
-                url: route.url,
-                handler: (context, props) => {
-                    const component = React.createElement(route.component, Object.assign({}, props));
-                    reactAdapter.render(
-                        context,
-                        component,
-                        {
-                            renderToStaticMarkup: appConfig.renderToStaticMarkup,
-                            template: appConfig.template || (x => x)
-                        }
-                    );
-                },
-                options: { argumentsAsObject: true }
-            };
+    const routes = appConfig.module.routes.map(_route => {
+        const route = getAppRoute(_route);
+        if (route.type === "handler") {
+            return getHandlerRoute(route, appConfig);
+        } else if (route.type === "react") {
+            return getReactRoute(route, appConfig);
+        } else if (route.type === "relay") {
+            return getRelayRoute(route, appConfig);
+        } else {
+            throw new Error("Unknown type. Route type must be handler, react or relay.");
+        }
     });
 
     router.add(routes);
