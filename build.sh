6dd3de318368f8814aa3ab352f881f7a33a1226c
Jeswin Kumar <jeswinpk@agilehead.com>
2015-12-23 22:08:48 +0530

    Source maps

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ find dist/ -name *.js -type f -exec cp {} {}.flow \;
 babel --no-babelrc --presets "es2015","react","stage-2" src/test/my-schema.js -o dist/test/my-schema.js
 babel --no-babelrc --presets "es2015","react","stage-2" src/scripts/updateSchema.js -o dist/scripts/updateSchema.js
 npm run update-schema
-babel src/ -d dist/ "$@"
+babel src/ -d dist/ "$@" --source-maps
